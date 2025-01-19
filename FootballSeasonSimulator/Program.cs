2079be3f23fa1b9669d2be002a400b6a865b87b5
Nick Hahn <grymm1990@gmail.com>
2025-01-19 14:56:45 -0600

    Add project files.

---
+++ FootballSeasonSimulator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FootballSeasonSimulator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //interface
+            Console.WriteLine("=========================");
+            Console.WriteLine("FOOTBALL SEASON SIMULATOR");
+            Console.WriteLine("=========================");
+
+            //Console.Clear();
+
+            //TODO data structure for teams, divisions, and conferences
+            //TODO team season generation based on league rules
+
+            //TODO basic number based interface
+
+            //TODO preconfiguration of team powers
+            // - offense
+            // - defense
+
+            //TODO preconfiguration of last season results for season generation
+
+
+            //TODO game simulation
+
+            //TODO week simulation
+
+            //TODO playoffs setup
+
+            //TODO playoffs simulation
+
+            //TODO results playout
+        }
+    }
+}
